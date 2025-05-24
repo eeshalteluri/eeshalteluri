fdb34d62e37b4154b4e2bc7d0b588a556ec0c098
eeshalteluri <eeshalteluri@gmail.com>
2025-05-24 16:29:21 +0530

    Full updated code of the SPA

--- components/BenefitCard.js
+++ components/BenefitCard.js
@@ -2,25 +2,25 @@ const BenefitCard = (title, description, icon, index) => {
     const isEven = index % 2 === 0;
 
     return `
-        <div class="min-w-[250px] md:w-[350px] h-[350px] flex-shrink-0 border border-secondary rounded p-6 flex flex-col justify-center items-center gap-4 bg-white shadow-lg">
+        <div class="min-w-[250px] md:w-[350px] h-[350px] flex-shrink-0 border border-secondary rounded p-6 flex flex-col justify-between items-end gap-4 bg-white shadow-lg">
             ${
                 isEven 
                 ? 
                 `
-                    <div>
-                        <h3>${title}</h3>
-                        <p>${description}</p>
+                    <div class="text-left flex flex-col gap-4">
+                        <h3 class="text-5xl font-extrabold">${title}</h3>
+                        <p class="max-w-[200px]">${description}</p>
                     </div>
 
-                    <img src="${icon}" alt="${title} icon" class="w-16 h-16" />
+                    <img src="${icon}" alt="${title} icon" class="w-28 h-28" />
                 `
                 :
                 `
-                    <img src="${icon}" alt="${title} icon" class="w-16 h-16" />
+                    <img src="${icon}" alt="${title} icon" class="w-28 h-28" />
 
-                    <div>
-                        <h3>${title}</h3>
-                        <p>${description}</p>
+                    <div class="text-left flex flex-col gap-4>
+                        <h3 class="text-5xl font-extrabold">${title}</h3>
+                        <p class="max-w-[200px]>${description}</p>
                     </div>
                 `
             }
